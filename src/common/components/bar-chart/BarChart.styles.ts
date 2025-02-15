bb38e3f6fa8132b8e878edbc449f27428407c229
gabi <70315572+Limgabi@users.noreply.github.com>
2025-02-15 20:02:19 +0900

    feat #29 : BarChart 컴포넌트 구현 및 스토리 추가

---
+++ src/common/components/bar-chart/BarChart.styles.ts
@@ -0,0 +1,22 @@
+import { css, keyframes } from '@emotion/react';
+
+export const barChartStyle = css`
+  overflow: hidden;
+  height: 6px;
+  background-color: #ebeef2;
+`;
+
+export const barChartBar = (value: number) => css`
+  height: 100%;
+  background-color: #4a5468;
+  animation: ${widthAnimation(value)} 2s ease-in-out forwards;
+`;
+
+const widthAnimation = (value: number) => keyframes`
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: ${value}%;
+  }
+`;
