3f646350d64116591e35c35e072d1ef02e99a47a
minh0518 <rubics@naver.com>
2025-02-17 21:33:29 +0900

    refactor #34 : 임시 데이터 형식 수정

--- src/features/sidebar/service/data.ts
+++ src/features/sidebar/service/data.ts
@@ -1,10 +1,10 @@
-import { SidebarListType } from '../types/sidebarInfoTypes';
+import { SidebarListType } from '../types/sidebar-Info-types';
 
 export const sidebarList: Record<'data', SidebarListType> = {
   data: [
-    { projectTitle: '토스 새소식 프로젝트', feedbackPages: [2, 3, 7, 8, 9, 11] },
-    { projectTitle: '소비요정', feedbackPages: [4, 6, 10, 12] },
-    { projectTitle: '주식 기부하기 이벤트', feedbackPages: [13, 15, 16, 17, 20] },
-    { projectTitle: '리브랜딩', feedbackPages: [25, 26, 27, 31, 32, 36, 38] },
+    { projectTitle: '토스 새소식 프로젝트', feedbackPages: ['2p', '3p', '7p', '8p', '9p', '11p'] },
+    { projectTitle: '소비요정', feedbackPages: ['4p', '6p', '10p', '12p'] },
+    { projectTitle: '주식 기부하기 이벤트', feedbackPages: ['13p', '15p', '16p', '17p', '20p'] },
+    { projectTitle: '리브랜딩', feedbackPages: ['25p', '26p', '27p', '31p', '32p', '36p', '38p'] },
   ],
 };
