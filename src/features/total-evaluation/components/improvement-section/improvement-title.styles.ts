b36badcc3a445f5729e965a8e10f9ad44f22b974
subsub-e <ab13696802@gmail.com>
2025-02-18 22:11:08 +0900

    style #25 : 문장 개선 타이틀 컴포넌트 퍼블리싱

---
+++ src/features/total-evaluation/components/improvement-section/improvement-title.styles.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+export const improvementTitleWrapper = css`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+
+export const improvementTitle = css`
+  color: #4a5468;
+  padding-top: 3px;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 36px;
+  letter-spacing: 0.24px;
+`;
---
+++ src/features/total-evaluation/components/improvement-section/improvement-title.tsx
@@ -0,0 +1,16 @@
+import CheckIcon from '../../../../assets/icons/checkIcon.svg?react';
+
+import * as styles from './improvement-title.styles';
+
+interface ImprovementTitleProps {
+  improvementTitle: string;
+}
+
+export default function ImprovementTitle({ improvementTitle }: ImprovementTitleProps) {
+  return (
+    <div css={styles.improvementTitleWrapper}>
+      <CheckIcon />
+      <span css={styles.improvementTitle}>{improvementTitle}</span>
+    </div>
+  );
+}
