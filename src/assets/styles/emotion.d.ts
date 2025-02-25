2dd05d126ec410937b0e1a28c32d40c039a53a96
임가비 <gabi520@naver.com>
2025-02-25 13:59:35 +0900

    style #63 : Colorsyetem 설정

--- src/assets/styles/emotion.d.ts
+++ src/assets/styles/emotion.d.ts
@@ -1,9 +1,12 @@
 import '@emotion/react';
 
+import { ColorsTypes } from '@/assets/styles/colors';
+
 import { FontsTypes } from './fonts';
 
 declare module '@emotion/react' {
   export interface Theme {
     fonts: FontsTypes;
+    colors: ColorsTypes;
   }
 }
--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,9 +1,12 @@
 import { Theme } from '@emotion/react';
 
+import { colors } from '@/assets/styles/colors';
+
 import { fonts } from './fonts';
 
 export const theme: Theme = {
   fonts,
+  colors,
 };
 
 export type ThemeType = typeof theme;
