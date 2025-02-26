102f5c670070e90a0d07e4c2d59d9b2cc54408ff
minh0518 <rubics@naver.com>
2025-02-26 20:42:20 +0900

    feat #67 : 임시 테스트용 코드 메인 페이지에서 제거

--- src/route.tsx
+++ src/route.tsx
@@ -3,50 +3,12 @@ import { createBrowserRouter } from 'react-router';
 import TotalEvalutionPage from '@/features/total-evaluation/total-evaluation-page';
 import UploadPage from '@/features/upload/upload-page';
 
-import { Button } from './common/components/button/Button';
-import Icon from './common/components/icon/icon';
-
 export const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <div>
-        <Button size="medium" usage="text" variant="purlple">
-          purple-L
-        </Button>
-
-        <Button size="small" usage="text" variant="sora">
-          sora-S
-        </Button>
-
-        <Button size="large" usage="icon" variant="sora" icon={<Icon name="pin" width={24} />} />
-
-        <Button
-          size="medium"
-          usage="icon"
-          variant="purlple"
-          icon={<Icon name="pin" width={24} />}
-        />
-
-        <Button
-          size="medium"
-          usage="multi"
-          icon={<Icon name="pin" width={24} />}
-          iconPosition="right"
-          variant="sora"
-        >
-          sora-multi-left-M
-        </Button>
-
-        <Button
-          size="small"
-          usage="multi"
-          icon={<Icon name="pin" width={24} />}
-          iconPosition="left"
-          variant="purlple"
-        >
-          purlple-multi-left-S
-        </Button>
+        <h1>메인 페이지</h1>
       </div>
     ),
   },
