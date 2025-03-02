73ed3111e7461514d7104352e318d9970dfc8235
minh0518 <rubics@naver.com>
2025-03-02 16:27:09 +0900

    feat #70 : 사이드바 열림 버튼 색상 및 위치 수정

--- src/features/total-evaluation/components/custom-buttons/sidebar-open-button.tsx
+++ src/features/total-evaluation/components/custom-buttons/sidebar-open-button.tsx
@@ -1,21 +1,26 @@
 import { ButtonHTMLAttributes, forwardRef } from 'react';
 import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md';
 
+import { useTheme } from '@emotion/react';
+
 import { BaseButton } from '@/common/components/button/base-button';
 
 import * as styles from './sidebar-control-button.styles';
 
 const SidebarOpenButton = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement>>(
-  (props, ref) => (
-    <BaseButton
-      css={styles.controlButtonCommonStyle}
-      style={{ backgroundColor: 'transparent' }}
-      {...props}
-      ref={ref}
-    >
-      <MdOutlineKeyboardDoubleArrowRight size={24} color="black" />
-    </BaseButton>
-  ),
+  (props, ref) => {
+    const theme = useTheme();
+    return (
+      <BaseButton
+        css={styles.controlButtonCommonStyle}
+        style={{ backgroundColor: 'transparent' }}
+        {...props}
+        ref={ref}
+      >
+        <MdOutlineKeyboardDoubleArrowRight size={24} color={theme.colors.GRAY[700]} />
+      </BaseButton>
+    );
+  },
 );
 SidebarOpenButton.displayName = 'SidebarOpenButton';
 
