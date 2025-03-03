f2bc3963cd202967b07c8fb1f1f13227ed45ad48
minh0518 <rubics@naver.com>
2025-03-04 01:00:12 +0900

    refactor #78 : 스타일 객체 폰트 분리

--- src/assets/styles/button.ts
+++ src/assets/styles/button.ts
@@ -9,56 +9,35 @@ export const BUTTON_SIZE = {
   text: {
     small: {
       padding: `${sizeToken.padding['xs_3']} ${sizeToken.padding['sm_2']}`,
-      fontSize: `${sizeToken.fontSize['xs_1']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
       borderRadius: `${sizeToken.borderRadius['xs_2']}`,
     },
     medium: {
       padding: `${sizeToken.padding['sm_2']} ${sizeToken.padding['md_3']}`,
-      fontSize: `${sizeToken.fontSize['s_1']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
       borderRadius: `${sizeToken.borderRadius['xs_1']}`,
     },
     large: {
       padding: `${sizeToken.padding['sm_2']} ${sizeToken.padding['lg_2']}`,
-      fontSize: `${sizeToken.fontSize['s_2']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
-      lineHeight: `${sizeToken.lineHeight['s_1']}`,
       borderRadius: `${sizeToken.borderRadius['xs_2']}`,
     },
     xxLarge: {
       padding: `${sizeToken.padding['md_2']} ${sizeToken.padding['lg_1']}`,
-      fontSize: `${sizeToken.fontSize['sm_1']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
       borderRadius: `${sizeToken.borderRadius['lg_1']}`,
     },
   },
   multi: {
     small: {
       padding: `${sizeToken.padding['xs_3']} ${sizeToken.padding['s_2']} ${sizeToken.padding['xs_3']} ${sizeToken.padding['sm_2']}`,
-      fontSize: `${sizeToken.fontSize['xs_1']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
       borderRadius: `${sizeToken.borderRadius['xs_1']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
       gap: `${sizeToken.gap['xs_1']}`,
     },
     medium: {
       padding: `${sizeToken.padding['s_2']} ${sizeToken.padding['md_1']} ${sizeToken.padding['s_2']} ${sizeToken.padding['md_3']}`,
-      fontSize: `${sizeToken.fontSize['s_1']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
       borderRadius: `${sizeToken.borderRadius['xs_2']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
       gap: `${sizeToken.gap['xs_1']}`,
     },
     large: {
       padding: `${sizeToken.padding['sm_1']} ${sizeToken.padding['md_3']} ${sizeToken.padding['sm_1']} ${sizeToken.padding['lg_2']}`,
-      fontSize: `${sizeToken.fontSize['s_2']}`,
-      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
       borderRadius: `${sizeToken.borderRadius['xs_2']}`,
-      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
       gap: `${sizeToken.gap['xs_1']}`,
     },
   },
@@ -74,6 +53,48 @@ export const BUTTON_SIZE = {
     large: {
       padding: `${sizeToken.padding['s_1']}`,
       borderRadius: `${sizeToken.borderRadius['s_1']}`,
+    },
+  },
+} as const;
+
+export const BUTTON_FONTS = {
+  text: {
+    small: {
+      fontSize: `${sizeToken.fontSize['xs_1']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
+    },
+    medium: {
+      fontSize: `${sizeToken.fontSize['s_1']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
+    },
+    large: {
+      fontSize: `${sizeToken.fontSize['s_2']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['s_1']}`,
+    },
+    xxLarge: {
+      fontSize: `${sizeToken.fontSize['sm_1']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_2']}`,
+    },
+  },
+  multi: {
+    small: {
+      fontSize: `${sizeToken.fontSize['xs_1']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
+    },
+    medium: {
+      fontSize: `${sizeToken.fontSize['s_1']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
+    },
+    large: {
+      fontSize: `${sizeToken.fontSize['s_2']}`,
+      fontWeight: `${sizeToken.fontWeight['xs_1']}`,
+      lineHeight: `${sizeToken.lineHeight['xs_1']}`,
     },
   },
 } as const;
@@ -153,19 +174,20 @@ export const BUTTON_VARIANTS = {
   },
 } as const;
 
-/* 각 카테고리별 사이즈 키 */
+/* 각 카테고리별 키값 */
+// 1. 사용처
 export type Usage = keyof typeof BUTTON_SIZE;
 
-// 사이즈
-export type Size = keyof (typeof BUTTON_SIZE)['text'];
-export type TextSize = Size;
-export type RestSize = Exclude<Size, 'xxLarge'>;
+// 2. 사이즈(+폰트에도 사용)
+export type SizeKey = keyof (typeof BUTTON_SIZE)['text'];
+export type TextSize = SizeKey;
+export type RestSize = Exclude<SizeKey, 'xxLarge'>;
 
-// 색상 테마
-export type Variant = keyof typeof BUTTON_VARIANTS;
-export type TextVariant = Exclude<Variant, 'icon'>;
-export type IconVariant = Extract<Variant, 'icon'>;
-export type RestVariant = Exclude<Variant, 'icon' | 'text'>;
+// 3. 색상 테마
+export type VariantKey = keyof typeof BUTTON_VARIANTS;
+export type TextVariant = Exclude<VariantKey, 'icon'>;
+export type IconVariant = Extract<VariantKey, 'icon'>;
+export type RestVariant = Exclude<VariantKey, 'icon' | 'text'>;
 
 /** 각 버튼의 경우 대한 타입 강제. Usage 타입에 따라 개별 타입 적용 */
 // 1. 텍스트만 있는 경우
--- src/common/components/button/button.styles.ts
+++ src/common/components/button/button.styles.ts
@@ -1,16 +1,18 @@
 import { css } from '@emotion/react';
 
 import {
+  BUTTON_FONTS,
   BUTTON_SIZE,
   BUTTON_VARIANTS,
   RestSize,
-  Size,
+  SizeKey,
   TextSize,
   Usage,
+  VariantKey,
 } from '@/assets/styles/button';
 
 const BUTTON_STYLE_SYSYEM = {
-  createSizeStyle: (size: Size, usage: Usage) => {
+  createSizeStyle: (size: SizeKey, usage: Usage) => {
     const sizeStyles: Record<Usage, ReturnType<typeof css>> = {
       text: css`
         padding: ${BUTTON_SIZE.text[size as TextSize].padding};
@@ -29,18 +31,18 @@ const BUTTON_STYLE_SYSYEM = {
     return sizeStyles[usage];
   },
 
-  createFontStyle: (size: Size, usage: Usage) => {
+  createFontStyle: (size: SizeKey, usage: Usage) => {
     const fontStyles: Record<Usage, ReturnType<typeof css>> = {
       text: css`
-        font-size: ${BUTTON_SIZE.text[size as TextSize].fontSize};
-        font-weight: ${BUTTON_SIZE.text[size as TextSize].fontWeight};
-        line-height: ${BUTTON_SIZE.text[size as TextSize].lineHeight};
+        font-size: ${BUTTON_FONTS.text[size as TextSize].fontSize};
+        font-weight: ${BUTTON_FONTS.text[size as TextSize].fontWeight};
+        line-height: ${BUTTON_FONTS.text[size as TextSize].lineHeight};
       `,
 
       multi: css`
-        font-size: ${BUTTON_SIZE.multi[size as RestSize].fontSize};
-        font-weight: ${BUTTON_SIZE.multi[size as RestSize].fontWeight};
-        line-height: ${BUTTON_SIZE.multi[size as RestSize].lineHeight};
+        font-size: ${BUTTON_FONTS.multi[size as RestSize].fontSize};
+        font-weight: ${BUTTON_FONTS.multi[size as RestSize].fontWeight};
+        line-height: ${BUTTON_FONTS.multi[size as RestSize].lineHeight};
       `,
 
       icon: css``,
@@ -68,7 +70,7 @@ const BUTTON_STYLE_SYSYEM = {
     }
   `,
 
-  createAdditionalStyle: (usage: Usage, size: Size) => {
+  createAdditionalStyle: (usage: Usage, size: SizeKey) => {
     const additionalStyle: Record<Usage, ReturnType<typeof css>> = {
       text: css``,
       multi: css`
@@ -84,11 +86,7 @@ const BUTTON_STYLE_SYSYEM = {
 };
 
 /* 최종 스타일 생성 */
-export const buttonsStyle = (
-  size: Size,
-  usage: Usage,
-  variant: keyof typeof BUTTON_VARIANTS,
-) => css`
+export const buttonsStyle = (size: SizeKey, usage: Usage, variant: VariantKey) => css`
   ${BUTTON_STYLE_SYSYEM.createSizeStyle(size, usage)};
   ${BUTTON_STYLE_SYSYEM.createFontStyle(size, usage)};
   ${BUTTON_STYLE_SYSYEM.createStateStyles(variant)};
