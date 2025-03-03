8852c6151ff20040f72b2daef1bf325583e3a1b9
gabi <70315572+Limgabi@users.noreply.github.com>
2025-03-03 12:43:06 +0900

    refactor #75 : 포트폴리오 전체 평가 데이터 및 구조 개선

--- src/features/total-evaluation/total-evaluation-page.tsx
+++ src/features/total-evaluation/total-evaluation-page.tsx
@@ -1,8 +1,4 @@
 import { evaluationData } from '@/features/total-evaluation/common/data';
-import EvaluationAnalyze from '@/features/total-evaluation/components/evaluation-analyze/evaluation-analyze';
-import EvaluationChart from '@/features/total-evaluation/components/evaluation-chart/evaluation-chart';
-import EvaluationSummary from '@/features/total-evaluation/components/evaluation-summary/evaluation-summary';
-import EvaluationTable from '@/features/total-evaluation/components/evaluation-table/evaluation-table';
 import EvaluationTitle from '@/features/total-evaluation/components/evaluation-title/evaluation-title';
 import FeedbackSidebar from '@/features/total-evaluation/components/feedback-sidebar/feedback-sidebar';
 import ImprovementSection from '@/features/total-evaluation/components/improvement-section/improvement-section';
@@ -11,42 +7,19 @@ import LogicalLeap from '@/features/total-evaluation/components/logical-leap/log
 import NestedList from '@/features/total-evaluation/components/nested-list/nested-list';
 import { EVALUATION_LABEL } from '@/features/total-evaluation/constants/evaluation-constant';
 
+import OverallEvaluation from './components/overall-evaluation/overall-evaluation';
+
 import * as styles from './total-evaluation-page.styles';
 
 export default function TotalEvalutionPage() {
-  const {
-    evaluationSummary,
-    overallEvaluationGrade,
-    evaluationItems,
-    strengths,
-    solutions,
-    improvementData,
-    positives,
-    negatives,
-    logicalLeaps,
-  } = evaluationData;
+  const { improvementData, positives, negatives, logicalLeaps } = evaluationData;
 
   return (
     <div css={styles.container}>
       <FeedbackSidebar />
 
       <div css={styles.totalEvaluationSection}>
-        <EvaluationSummary evaluationSummary={evaluationSummary} />
-        <EvaluationChart
-          overallEvaluationGrade={overallEvaluationGrade}
-          evaluationItems={evaluationItems}
-        />
-        <EvaluationTable evaluationItems={evaluationItems} />
-
-        <section css={styles.evaluationSection('2.2rem')}>
-          <EvaluationTitle title={EVALUATION_LABEL['strengths']} icon={<span>🔥</span>} />
-          <EvaluationAnalyze analysisItems={strengths} />
-        </section>
-
-        <section css={styles.evaluationSection('2.2rem')}>
-          <EvaluationTitle title={EVALUATION_LABEL['solutions']} icon={<span>👀</span>} />
-          <EvaluationAnalyze analysisItems={solutions} />
-        </section>
+        <OverallEvaluation />
 
         <section css={styles.evaluationSection('1.6rem')}>
           <ImprovementTitle improvementTitle={improvementData.title} />
