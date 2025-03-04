6e98c60d443693ba58d5a2cfc8e629f2d07294c0
Jeongki <qq8721443@naver.com>
2025-03-04 15:41:15 +0900

    [#60] - 업로드 페이지 스타일 적용
    * feat #60 : 업로드 페이지 마크업 수정 및 스타일 적용
    
    * refactor #60 : FileUpload, Dropzone 컴포넌트 통합
    
    * chore #60 : 업로드 컴포넌트 내부 아이콘 파일 추가
    
    * feat #60 : 업로드 컴포넌트 디자인 적용
    
    * refactor #60 : 불필요 스타일 코드 제거
    
    * refactor #60 : 임시 SVG 코드 제거
    
    * refactor #60 : 용도에 맞는 시맨틱 태그 사용하도록 변경

--- src/features/upload/components/file-upload/file-upload.tsx
+++ src/features/upload/components/file-upload/file-upload.tsx
@@ -4,6 +4,10 @@ import { Controller, FieldValues, SubmitErrorHandler, useForm } from 'react-hook
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
+import Icon from '@/common/components/icon/icon';
+
+import FileUploadButton from './file-upload-button';
+
 import * as styles from './file-upload.styles';
 
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
@@ -25,9 +29,19 @@ export default function FileUpload({ onSubmit }: FileUploadProps) {
     handleSubmit,
     control,
     formState: { errors },
+    setValue,
   } = useForm({
     resolver: zodResolver(schema),
     mode: 'onChange',
+  });
+
+  const { acceptedFiles, getInputProps, getRootProps, isDragActive, open } = useDropzone({
+    noClick: true,
+    onDrop: (files) => {
+      setValue('file', files[0]);
+
+      handleSubmit(onSubmit, onSubmitError)();
+    },
   });
 
   const onSubmitError: SubmitErrorHandler<FieldValues> = (errors) => {
@@ -35,69 +49,48 @@ export default function FileUpload({ onSubmit }: FileUploadProps) {
   };
 
   return (
-    <form css={styles.form} onSubmit={handleSubmit(onSubmit, onSubmitError)}>
+    <form onSubmit={handleSubmit(onSubmit, onSubmitError)}>
       <Controller
         name="file"
         control={control}
         render={({ field }) => (
-          <Dropzone
-            onChange={(files) => {
-              field.onChange(files);
+          <div {...getRootProps({ css: styles.container })}>
+            <input
+              {...getInputProps({
+                accept: 'application/pdf',
+                onChange: (e) => {
+                  const { files } = e.target;
 
-              handleSubmit(onSubmit, onSubmitError)();
-            }}
-          />
+                  field.onChange(files?.[0]);
+
+                  handleSubmit(onSubmit, onSubmitError)();
+                },
+              })}
+            />
+            <Icon name="add-document" width={50} />
+
+            <div css={styles.description}>
+              {isDragActive ? (
+                <p>파일 업로드하기</p>
+              ) : (
+                acceptedFiles.length === 0 && (
+                  <p>
+                    <strong>PDF를 업로드해주세요.</strong>
+                    <wbr /> 최대 50mb까지 가능해요.
+                  </p>
+                )
+              )}
+
+              {!isDragActive && <div>{acceptedFiles?.[0]?.name}</div>}
+            </div>
+
+            <FileUploadButton type="button" onClick={open}>
+              PDF 업로드하기
+            </FileUploadButton>
+            {errors.file && <p css={styles.errorText}>{errors.file.message?.toString()}</p>}
+          </div>
         )}
       />
-      {errors.file && <div css={styles.errorText}>{errors.file.message?.toString()}</div>}
     </form>
-  );
-}
-FileUpload.displayName = 'FileUpload';
-
-function Dropzone({ onChange }: { onChange?: (...event: unknown[]) => void }) {
-  const { acceptedFiles, getInputProps, getRootProps, isDragActive, open } = useDropzone({
-    noClick: true,
-    onDrop: (files) => {
-      onChange?.(files[0]);
-    },
-  });
-
-  return (
-    <div {...getRootProps()} css={styles.container}>
-      <input
-        {...getInputProps({
-          accept: 'application/pdf',
-        })}
-      />
-
-      <div css={styles.preview}>
-        {isDragActive ? (
-          <p>파일 업로드하기</p>
-        ) : (
-          acceptedFiles.length === 0 && <p>포트폴리오 PDF 업로드하기</p>
-        )}
-
-        {!isDragActive && <div>{acceptedFiles?.[0]?.name}</div>}
-      </div>
-
-      <button type="button" onClick={open} css={styles.uploadButton}>
-        {/* + svg 임시 사용 */}
-        <PlugSvg />
-      </button>
-    </div>
-  );
-}
-
-function PlugSvg() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M20.318 10.8H13.318V3.79998C13.318 3.48172 13.1916 3.17649 12.9665 2.95145C12.7415 2.7264 12.4363 2.59998 12.118 2.59998C11.7998 2.59998 11.4945 2.7264 11.2695 2.95145C11.0444 3.17649 10.918 3.48172 10.918 3.79998V10.8H3.91802C3.59976 10.8 3.29453 10.9264 3.06949 11.1514C2.84445 11.3765 2.71802 11.6817 2.71802 12C2.71802 12.3182 2.84445 12.6235 3.06949 12.8485C3.29453 13.0735 3.59976 13.2 3.91802 13.2H10.918V20.2C10.918 20.5182 11.0444 20.8235 11.2695 21.0485C11.4945 21.2735 11.7998 21.4 12.118 21.4C12.4363 21.4 12.7415 21.2735 12.9665 21.0485C13.1916 20.8235 13.318 20.5182 13.318 20.2V13.2H20.318C20.6363 13.2 20.9415 13.0735 21.1665 12.8485C21.3916 12.6235 21.518 12.3182 21.518 12C21.518 11.6817 21.3916 11.3765 21.1665 11.1514C20.9415 10.9264 20.6363 10.8 20.318 10.8Z"
-        fill="#8B95A1"
-      />
-    </svg>
   );
 }
--- src/features/upload/upload-page.styles.ts
+++ src/features/upload/upload-page.styles.ts
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
 
+import { withTheme } from '@/common/utils/with-theme';
+
 export const container = css`
   display: flex;
   align-items: center;
@@ -7,29 +9,17 @@ export const container = css`
   flex-direction: column;
   width: 100dvw;
   height: 100dvh;
+  padding: 0 3.2rem;
 `;
 
-export const logo = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 11.7rem;
-  height: 6.5rem;
-  color: #969696;
-  font-size: 2.4rem;
-  font-weight: 600;
-  text-align: center;
-  background-color: #d9d9d9;
-  border-radius: 2.4rem;
-  font-style: normal;
-  letter-spacing: -0.048rem;
-`;
-
-export const title = css`
-  margin: 5.6rem 0;
-  color: #000;
-  font-size: 4rem;
-  font-weight: 600;
-  text-align: center;
-  letter-spacing: -0.08rem;
-`;
+export const title = withTheme(
+  (theme) => css`
+    margin-bottom: 5rem;
+    background: linear-gradient(90deg, #fff 0%, #ace4fa 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-align: center;
+    ${theme.fonts.HEADLINE.HEAD2}
+  `,
+);
