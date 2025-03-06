da5d426ac203cd1c75314d2b8029a50af733994f
subsub-e <ab13696802@gmail.com>
2025-03-07 03:09:56 +0900

    refactor #81 : Typography 변경에 따른 수정

--- src/assets/styles/fonts.ts
+++ src/assets/styles/fonts.ts
@@ -16,38 +16,38 @@ const generateTypography = (
 
 export const fonts = {
   HEADLINE: {
-    HEAD1: generateTypography(3.6, 700, 140, 0.036),
-    HEAD2: generateTypography(3.2, 700, 140, 0.032),
-    HEAD3: generateTypography(2.6, 700, 140, 0.026),
-    HEAD4: generateTypography(2.4, 700, 140, 0.024),
-    HEAD5: generateTypography(2.2, 600, 140, 0.022),
-    HEAD6: generateTypography(2, 700, 140, 0.02),
+    HEAD1: generateTypography(3.6, 625, 140, 0.036),
+    HEAD2: generateTypography(3.2, 625, 140, 0.032),
+    HEAD3: generateTypography(2.6, 625, 140, 0.026),
+    HEAD4: generateTypography(2.4, 625, 140, 0.024),
+    HEAD5: generateTypography(2.2, 525, 140, 0.022),
+    HEAD6: generateTypography(2, 625, 140, 0.02),
   },
 
   SUBTITLE: {
-    SUB1_SB: generateTypography(2, 600, 140),
-    SUB1_M: generateTypography(2, 500, 140),
-    SUB2_B: generateTypography(1.8, 700, 140),
-    SUB2_SB: generateTypography(1.8, 600, 140),
-    SUB3_B: generateTypography(1.6, 700, 140),
-    SUB3_SB: generateTypography(1.6, 600, 140),
-    SUB4_SB: generateTypography(1.5, 600, 140),
-    SUB5_SB: generateTypography(1.4, 600, 140),
-    SUB5_M: generateTypography(1.4, 500, 140),
+    SUB1_SB: generateTypography(2, 525, 140),
+    SUB1_M: generateTypography(2, 425, 140),
+    SUB2_B: generateTypography(1.8, 625, 140),
+    SUB2_SB: generateTypography(1.8, 525, 140),
+    SUB3_B: generateTypography(1.6, 625, 140),
+    SUB3_SB: generateTypography(1.6, 525, 140),
+    SUB4_SB: generateTypography(1.5, 525, 140),
+    SUB5_SB: generateTypography(1.4, 525, 140),
+    SUB5_M: generateTypography(1.4, 425, 140),
   },
 
   BODY: {
-    BODY1_R: generateTypography(1.8, 400, 160, -0.018),
-    BODY2_M: generateTypography(1.6, 500, 170, -0.016),
-    BODY2_R: generateTypography(1.6, 400, 170, -0.016),
-    BODY3_R: generateTypography(1.4, 400, 160, -0.014),
-    BODY4_M: generateTypography(1.3, 500, 150, -0.013),
-    BODY5_M: generateTypography(1.2, 500, 150, -0.012),
+    BODY1_R: generateTypography(1.8, 325, 160, -0.018),
+    BODY2_M: generateTypography(1.6, 425, 170, -0.016),
+    BODY2_R: generateTypography(1.6, 325, 170, -0.016),
+    BODY3_R: generateTypography(1.4, 325, 160, -0.014),
+    BODY4_M: generateTypography(1.3, 425, 150, -0.013),
+    BODY5_M: generateTypography(1.2, 425, 150, -0.012),
   },
 
   CAPTION: {
-    CAPTION1_SB: generateTypography(1.4, 600, 150),
-    CAPTION1_M: generateTypography(1.4, 500, 150),
+    CAPTION1_SB: generateTypography(1.4, 525, 150),
+    CAPTION1_M: generateTypography(1.4, 425, 150),
   },
 } as const;
 
