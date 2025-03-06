efbf1dc86a8e3c66a230dcb0e187d4fa8f09d06f
gabi <70315572+Limgabi@users.noreply.github.com>
2025-03-06 20:14:25 +0900

    style #92 : 랜딩페이지 - 포트폴리오 업로드 페이지로 라우팅하는 섹션 퍼블리싱

--- src/features/landing/landing-page.tsx
+++ src/features/landing/landing-page.tsx
@@ -1,11 +1,15 @@
 import FAQ from './components/frequently-asked-questions/frequently-asked-questions';
+import RoutingBottomSection from './components/routing-section/routing-bottom-section';
+import RoutingStartSection from './components/routing-section/routing-start-section';
 
 import * as styles from './landing-page.styles';
 
 export default function LandingPage() {
   return (
     <div css={styles.landingPage}>
+      <RoutingStartSection />
       <FAQ />
+      <RoutingBottomSection />
     </div>
   );
 }
