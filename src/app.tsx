b2150580bffb9410be2c55ab6dc48194beb27876
minh0518 <rubics@naver.com>
2025-03-08 17:07:09 +0900

    chore #102 : Tanstack-query 데브툴즈 적용

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router';
 import { globalStyles } from '@assets/styles/global-styles';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { theme } from './assets/styles/theme';
 import { router } from './route';
@@ -12,6 +13,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={process.env.NODE_ENV === 'development'} />
       <ThemeProvider theme={theme}>
         <Global styles={globalStyles} />
         <RouterProvider router={router} />
