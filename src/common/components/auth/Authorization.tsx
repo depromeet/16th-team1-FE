9a85d7995736e91093a2ca14572282bd060ecf69
minh0518 <rubics@naver.com>
2025-03-09 03:32:39 +0900

    fix #117 : 로그인 리다이렉션 로직 제거

--- src/common/components/auth/Authorization.tsx
+++ src/common/components/auth/Authorization.tsx
@@ -1,27 +1,10 @@
-import { ReactNode, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { ReactNode } from 'react';
 
-import Cookies from 'js-cookie';
-
-import { PAGE_URL } from '@/common/constants/path';
 interface AuthorizationProps {
   children: ReactNode;
 }
 
-// 어디서든 refreshToken이 있으면 재발급
-// 없으면 튕겨내면 될듯?
 function Authorization({ children }: AuthorizationProps) {
-  const navigate = useNavigate();
-  const refreshToken = Cookies.get('aaa');
-
-  useEffect(() => {
-    if (refreshToken) {
-      // 토근 발급
-    } else {
-      navigate(PAGE_URL.Login);
-    }
-  }, [refreshToken]);
-
   return <>{children}</>;
 }
 
