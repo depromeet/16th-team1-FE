dafc667c58ed6a2c87c5dc556850c0fc5ad6b1ac
minh0518 <rubics@naver.com>
2025-03-09 01:40:04 +0900

    feat #84 : 인증 기반 라우팅 구현

--- src/features/login/components/custom-buttons/google-auth-button.tsx
+++ src/features/login/components/custom-buttons/google-auth-button.tsx
@@ -4,8 +4,11 @@ import Icon from '@/common/components/icon/icon';
 import * as styles from './google-auth-button.styles';
 
 function GoogleAuthButton() {
+  const handleLoginButtonClick = () => {
+    window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`;
+  };
   return (
-    <BaseButton>
+    <BaseButton onClick={handleLoginButtonClick}>
       <div css={styles.buttonWrapper}>
         <Icon name="google" />
         <p>Google 계정으로 로그인</p>
