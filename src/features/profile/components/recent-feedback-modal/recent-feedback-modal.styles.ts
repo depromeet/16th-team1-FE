6210445e4e8c72e6fab36d154c986b33721780e7
subsub-e <ab13696802@gmail.com>
2025-03-08 22:14:09 +0900

    refactor #81 : recent-feedback-modal 수정

--- src/features/profile/components/recent-feedback-modal/recent-feedback-modal.styles.ts
+++ src/features/profile/components/recent-feedback-modal/recent-feedback-modal.styles.ts
@@ -2,89 +2,74 @@ import { css } from '@emotion/react';
 
 import { withTheme } from '@/common/utils/with-theme';
 
-export const modalContentWrapper = withTheme(
-  (theme) => css`
-    display: flex;
-    align-items: flex-start;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    width: 558px;
-    padding: 2.4rem 1.6rem 2.8rem;
-    background: ${theme.colors.GRAY[990]};
-    transform: translate(-50%, -50%);
-    flex-direction: column;
-    gap: 1.4rem;
-    border-radius: 1.6rem;
-    box-shadow: 0 0.4rem 1.05rem 0 rgb(0 0 0 / 82%);
-    outline: none;
-  `,
-);
-
 export const modalHeader = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
   align-self: stretch;
+  padding: 4rem 4rem 1.6rem;
 `;
 
 export const title = withTheme(
   (theme) => css`
-    ${theme.fonts.SUBTITLE.SUB3_SB}
-    padding: 0 10px;
+    ${theme.fonts.SUBTITLE.SUB2_SB}
+    color: ${theme.colors.GRAY[400]};
+  `,
+);
+
+export const modalDescription = css`
+  display: flex;
+  align-items: flex-start;
+  position: relative;
+  z-index: 1;
+  padding: 2.4rem 4rem 2.8rem;
+  overflow-y: auto;
+  flex-direction: column;
+  gap: 2.4rem;
+  align-self: stretch;
+`;
+
+export const dataWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+  gap: 1.4rem;
+`;
+
+export const yearAndMonth = withTheme(
+  (theme) => css`
+    ${theme.fonts.SUBTITLE.SUB3_B}
     color: ${theme.colors.GRAY[200]};
   `,
 );
 
-export const closeIcon = css`
-  display: flex;
-  align-items: center;
-  margin: 0.8rem 1rem;
-  gap: 1rem;
-`;
-
-export const modalDescription = css`
+export const feedbackGap = css`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 0.8rem;
-  align-self: stretch;
+  gap: 0.6rem;
 `;
 
-export const dataWrapper = (isChosen: boolean) =>
-  withTheme(
-    (theme) => css`
-      display: flex;
-      align-items: center;
-      padding: 0.6rem 1rem;
-      gap: 4rem;
-      align-self: stretch;
-      cursor: pointer;
-
-      ${isChosen &&
-      `border-radius: 8px;
-      background-color: ${theme.colors.GRAY[950]};`}
-
-      ${!isChosen &&
-      `&:hover {
-        border-radius: 8px;
-        background-color: ${theme.colors.GRAY[900]};
-      }`}
-    `,
-  );
+export const totalDescriptionWrapper = css`
+  display: flex;
+  align-items: flex-start;
+  padding: 0.8rem;
+  align-self: stretch;
+  gap: 4rem;
+`;
 
 export const date = withTheme(
   (theme) => css`
-    ${theme.fonts.BODY.BODY3_R}
+    ${theme.fonts.BODY.BODY2_M};
     color: ${theme.colors.GRAY[400]};
+    padding-right: 0.8rem;
   `,
 );
 
 export const descriptionWrapper = css`
   display: flex;
   align-items: center;
-  gap: 0.7rem;
-  align-self: stretch;
+  gap: 0.4rem;
 `;
 
 export const description = withTheme(
@@ -93,3 +78,28 @@ export const description = withTheme(
     color: ${theme.colors.GRAY[300]};
   `,
 );
+
+export const shadow = (isShow: boolean) => css`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
+  height: 10.1rem;
+  background: linear-gradient(180deg, rgb(32 32 40 / 0%) 0%, #202028 100%);
+  transition: opacity 0.2s ease;
+  border-radius: 2.4rem;
+  opacity: ${isShow ? 1 : 0};
+`;
+
+export const scrollbar = css`
+  ::-webkit-scrollbar {
+    z-index: 999;
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: rgb(0 0 0 / 30%);
+    border-radius: 4px;
+  }
+`;
--- src/features/profile/components/recent-feedback-modal/recent-feedback-modal.tsx
+++ src/features/profile/components/recent-feedback-modal/recent-feedback-modal.tsx
@@ -1,32 +1,52 @@
+import { useState } from 'react';
+
+import { colors } from '@/assets/styles/colors';
 import Icon from '@/common/components/icon/icon';
 import Modal from '@/common/components/modal/base-modal';
 
 import { feedbackData } from '../../common/data';
+import { parseFeedbackData } from '../../util/parse-feedback-data';
 
 import * as styles from './recent-feedback-modal.styles';
 
 export default function RecentFeedbackModal() {
+  const parseData = parseFeedbackData(feedbackData);
+
+  const [showShadow, setShowShadow] = useState(true);
+
+  const handleScroll = (e: React.UIEvent) => {
+    const isAtBottom =
+      e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.clientHeight;
+    setShowShadow(!isAtBottom);
+  };
+
   return (
-    <Modal.Root>
-      <Modal.Content css={styles.modalContentWrapper} aria-describedby={undefined}>
-        <div css={styles.modalHeader}>
-          <Modal.Title css={styles.title}>최근 피드백</Modal.Title>
-          <Modal.Close asChild>
-            <Icon name="close" customStyle={styles.closeIcon} />
-          </Modal.Close>
-        </div>
-        <div css={styles.modalDescription}>
-          {feedbackData.map((data) => (
-            <div key={data.id} css={styles.dataWrapper(data.isChosen)}>
-              <span css={styles.date}>{data.date}</span>
-              <div css={styles.descriptionWrapper}>
-                <Icon name="feedback" />
-                <span css={styles.description}>{data.description}</span>
-              </div>
+    <Modal.Root width="55.8rem" height="47.1rem" backgroundColor={colors.GRAY[990]}>
+      <header css={styles.modalHeader}>
+        <Modal.Title css={styles.title}>최근 피드백</Modal.Title>
+        <Modal.Close asChild>
+          <Icon name="close" />
+        </Modal.Close>
+      </header>
+      <main css={styles.modalDescription} onScroll={handleScroll}>
+        {Object.entries(parseData).map(([yearMonth, feedbackItems]) => (
+          <div key={yearMonth} css={styles.dataWrapper}>
+            <h3 css={styles.yearAndMonth}>{yearMonth}</h3>
+            <div css={styles.feedbackGap}>
+              {feedbackItems.map((feedback) => (
+                <article key={feedback.feedbackId} css={styles.totalDescriptionWrapper}>
+                  <span css={styles.date}>{feedback.date}일</span>
+                  <div css={styles.descriptionWrapper}>
+                    <Icon name="feedback" />
+                    <span css={styles.description}>{feedback.title}</span>
+                  </div>
+                </article>
+              ))}
             </div>
-          ))}
-        </div>
-      </Modal.Content>
+          </div>
+        ))}
+        <div css={styles.shadow(showShadow)} />
+      </main>
     </Modal.Root>
   );
 }
