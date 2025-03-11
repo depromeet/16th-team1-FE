09b71b95c35bc94df4d92a5a423620ea1096b625
minh0518 <rubics@naver.com>
2025-03-11 23:51:07 +0900

    chore #119 : 일반 http api요청 프록시를 위해 baseURL 초기화

--- src/common/services/service-config.ts
+++ src/common/services/service-config.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_API_BASE_URL, // http://211.188.53.83:8080
+  baseURL: '',
 });
