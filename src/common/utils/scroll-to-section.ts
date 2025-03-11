8c5ff74b1de818cf322122b052de8ddbba7e10a9
minh0518 <rubics@naver.com>
2025-03-12 00:10:38 +0900

    chore #131 : scrollToSection함수 공통 유틸 함수로 분리

--- src/common/utils/scroll-to-section.ts
+++ src/common/utils/scroll-to-section.ts
@@ -1,7 +1,21 @@
 /** id가 지정된 요소로 스크롤하는 유틸 함수 */
+import gsap from 'gsap';
+
 export const scrollToSection = (id: string) => {
   const element = document.getElementById(id);
-  if (element) {
+
+  if (!element) return;
+
+  if (id === 'help-section') {
+    const projectsContainer = document.getElementById('projects-container');
+    // 기존 scrollIntoView 메서드로는 중간 가로 스크롤 영역 때문에 제대로 동작하지 않음.
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: {
+        y: element.offsetTop + (projectsContainer?.clientWidth || 0) * 4,
+      },
+    });
+  } else {
     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 };
