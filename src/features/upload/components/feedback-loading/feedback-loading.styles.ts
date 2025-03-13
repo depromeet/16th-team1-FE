c818baef1f6e7ad908e0c80e80e0f82a5ac0ecc2
subsub-e <ab13696802@gmail.com>
2025-03-13 23:48:29 +0900

    style #128 : 포트폴리오 피드백 대기 화면 반응형 작업 및 사진, 아이콘 추가

--- src/features/upload/components/feedback-loading/feedback-loading.styles.ts
+++ src/features/upload/components/feedback-loading/feedback-loading.styles.ts
@@ -1,51 +1,16 @@
 import { css } from '@emotion/react';
 
-import { withTheme } from '@/common/utils/with-theme';
-
-export const contentWrapper = css`
+export const feedbackLoading = css`
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #18171d;
-`;
 
-export const title = withTheme(
-  (theme) => css`
-    ${theme.fonts.HEADLINE.HEAD2};
-    background: ${theme.colors.GRADIENT[300]};
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-  `,
-);
+  @media (width < 970px) and (width >= 768px) {
+    padding: 0 2.4rem;
+  }
 
-export const description = withTheme(
-  (theme) => css`
-    ${theme.fonts.SUBTITLE.SUB1_M}
-    color: ${theme.colors.GRAY[400]};
-  `,
-);
-
-export const exampleDescription = withTheme(
-  (theme) => css`
-    ${theme.fonts.SUBTITLE.SUB3_SB};
-    color: rgb(255 255 255 / 54%);
-  `,
-);
-
-export const floating = css`
-  animation: floating 2s cubic-bezier(0.4, 0, 0.6, 1) infinite 300ms;
-
-  @keyframes floating {
-    0% {
-      transform: translateY(0);
-    }
-
-    50% {
-      transform: translateY(-0.5rem);
-    }
-
-    100% {
-      transform: translateY(0);
-    }
+  @media (width < 768px) {
+    padding: 0 2rem;
   }
 `;
