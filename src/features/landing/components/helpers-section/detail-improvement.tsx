a451f74bcdf4241306a62aee07d37cf5c60a308b
gabi <70315572+Limgabi@users.noreply.github.com>
2025-03-17 00:51:36 +0900

    [#139] - 랜딩 페이지 인터랙션 적용 (#143)
    * chore #139 : 스크롤 감지를 위한 react-intersection-observer 설치
    
    * style #139 : 랜딩 페이지 인터랙션 적용
    
    * refactor #139 : fade in 애니메이션 관리하는 FadeInDiv 컴포넌트 구현 및 적용

--- src/features/landing/components/helpers-section/detail-improvement.tsx
+++ src/features/landing/components/helpers-section/detail-improvement.tsx
@@ -1,7 +1,9 @@
 import { useState, useMemo, useCallback } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { theme } from '@/assets/styles/theme';
 import Icon from '@/common/components/icon/icon';
+import FadeInDiv from '@/common/components/interaction/fade-in-div';
 import useDeviceType from '@/common/hooks/use-device-type';
 
 import { detailImprovementData } from '../../common/data';
@@ -9,6 +11,11 @@ import { detailImprovementData } from '../../common/data';
 import * as styles from './detail-improvement.styles';
 
 export default function DetailImprovement() {
+  const { ref: detailImprovementRef, inView: detailImprovementInView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
   const { isMobile } = useDeviceType();
 
   const [currentImprovementKey, setCurrentImprovementKey] = useState(
@@ -25,7 +32,12 @@ export default function DetailImprovement() {
   }, []);
 
   return (
-    <div css={styles.detailImprovementWrapper}>
+    <FadeInDiv
+      ref={detailImprovementRef}
+      inView={detailImprovementInView}
+      delay={0.5}
+      additionalStyles={styles.detailImprovementWrapper}
+    >
       <ul css={styles.improvementItemWrapper}>
         {detailImprovementData.map(({ title }) => (
           <li
@@ -66,6 +78,6 @@ export default function DetailImprovement() {
           </>
         )}
       </div>
-    </div>
+    </FadeInDiv>
   );
 }
