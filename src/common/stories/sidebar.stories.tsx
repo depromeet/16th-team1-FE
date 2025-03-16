c3ef6d55c21c9949b81bf060053f57d429c3a5e8
subsub-e <ab13696802@gmail.com>
2025-03-17 01:46:47 +0900

    refactor #123 : api 수정에 따른 sidebar 스토리 더미데이터 수정

--- src/common/stories/sidebar.stories.tsx
+++ src/common/stories/sidebar.stories.tsx
@@ -1,24 +1,23 @@
-// import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
-// import FeedbackSidebar from '@/features/total-evaluation/components/feedback-sidebar/feedback-sidebar';
-// import { ProjectEvaluationType } from '@/features/total-evaluation/services/use-get-portfolio-feedback';
+import FeedbackSidebar from '@/features/total-evaluation/components/feedback-sidebar/feedback-sidebar';
+import { ProjectEvaluationType } from '@/features/total-evaluation/services/use-get-portfolio-feedback';
 
-// import { dummyEvaluationData } from './dummy-data/sidebar';
+import { dummyEvaluationData } from './dummy-data/sidebar';
 
-// const meta: Meta<typeof FeedbackSidebar> = {
-//   title: 'Components/FeedbackSidebar',
-//   component: FeedbackSidebar,
-// };
+const meta: Meta<typeof FeedbackSidebar> = {
+  title: 'Components/FeedbackSidebar',
+  component: FeedbackSidebar,
+};
 
-// export default meta;
+export default meta;
 
-// type Story = StoryObj<typeof FeedbackSidebar>;
+type Story = StoryObj<typeof FeedbackSidebar>;
 
-// export const Default: Story = (args: { projectEvaluation: ProjectEvaluationType[] }) => {
-//   return <FeedbackSidebar {...args} />;
-// };
+export const Default: Story = (args: { projectEvaluation: ProjectEvaluationType[] }) => {
+  return <FeedbackSidebar {...args} />;
+};
 
-// Default.args = {
-//   projectEvaluation: dummyEvaluationData,
-// eslint-disable-next-line storybook/default-exports
-// };
+Default.args = {
+  projectEvaluation: dummyEvaluationData,
+};
