c9f84a1035ed6d6a7144d3b42357990402397944
gabi <70315572+Limgabi@users.noreply.github.com>
2025-03-19 21:13:16 +0900

    refactor #158 : 포트폴리오 피드백 조회 FallbackBoundary 적용

--- src/features/total-evaluation/total-evaluation-page.tsx
+++ src/features/total-evaluation/total-evaluation-page.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/common/components/button/Button';
 import FallbackBoundary from '@/common/components/fallback-boundary/fallback-boundary';
+import Skeleton from '@/common/components/skeleton/skeleton';
 import FeedbackSidebar from '@/features/total-evaluation/components/feedback-sidebar/feedback-sidebar';
 import ProjectEvaluationList from '@/features/total-evaluation/components/project-evaluation/project-evaluation-list';
 
@@ -11,7 +13,7 @@ export default function TotalEvaluationPage() {
     <div css={styles.container}>
       <FeedbackSidebar />
 
-      <FallbackBoundary>
+      <FallbackBoundary suspense={fallbacks.suspense} error={fallbacks.error}>
         <div css={styles.totalEvaluationSection}>
           <OverallEvaluation />
           <ProjectEvaluationList />
@@ -20,3 +22,23 @@ export default function TotalEvaluationPage() {
     </div>
   );
 }
+
+const fallbacks = {
+  suspense: {
+    fallbackUI: (
+      <div css={styles.fallbackWrapper}>
+        <Skeleton />
+      </div>
+    ),
+  },
+  error: {
+    fallbackUI: (onReset: VoidFunction) => (
+      <div css={styles.fallbackWrapper}>
+        <Button size="xLarge" usage="text" variant="primary" onClick={onReset}>
+          다시 시도
+        </Button>
+      </div>
+      // <Toast name="getFeedbackFailure" open={true} setOpen={setToastOpen} onClick={onReset} />
+    ),
+  },
+};
