a90659122c019a3c3efc4b88b2b4f69788ee823d
minh0518 <rubics@naver.com>
2025-03-19 20:58:01 +0900

    fix #119 : 개발 strictmode 2번 호출 이슈 대응

--- src/common/hooks/use-auth.ts
+++ src/common/hooks/use-auth.ts
@@ -6,12 +6,13 @@ import { AuthServices } from '../services/auth';
 export const useAuth = () => {
   const navigate = useNavigate();
   useEffect(() => {
-    const endPoint = '/api/v1/reissue';
     const authCylcle = async () => {
       try {
-        await AuthServices.postReIssue(endPoint);
-        await AuthServices.getUserInfo('');
-        AuthServices.updateUserinfo();
+        // 전역 상태로 유저 정보 확인
+
+        await AuthServices.postReIssue();
+        // await AuthServices.getUserInfo('');
+        // AuthServices.updateUserinfo();
       } catch (e: unknown) {
         // 위의 기본 인증 싸이클에 하나라도 에러가 터진다면 로그인 페이지로 리다이렉션
         navigate('/login');
--- src/common/services/auth.ts
+++ src/common/services/auth.ts
@@ -1,33 +1,48 @@
-import axios from 'axios';
+import { AxiosResponse } from 'axios';
 
 import { axiosInstance } from './service-config';
 import { ReIssue } from '../types/auth';
 
-export const AuthServices = {
-  postReIssue: async (endPoint: string) => {
-    const response = await axiosInstance.post<ReIssue>(endPoint);
-    const { accessToken, expirationTime } = response.data.result;
+const END_POINT = '/api/v1/reissue';
 
-    if (accessToken && expirationTime) {
-      AuthServices.enrollAuthorizationHeader(accessToken);
-      AuthServices.slientRefresh(expirationTime);
+let refreshTokenPromise: Promise<AxiosResponse<ReIssue, unknown>> | undefined;
+
+export const AuthServices = {
+  postReIssue: async () => {
+    if (refreshTokenPromise !== undefined) return;
+
+    /** RTR 환경에서 strtictMode로 인한 API 2번 호출 문제 대응
+     * - 에러의 경우 상위 컨텍스트에서 처리하고 있기에 별도로 catch를 작성하지 않았으며
+     * - 그럼에도 try finally를 적용한 이유는 에러가 발생했을 때도 refreshTokenPromise = undefined; 를 실행하기 위함
+     */
+    try {
+      refreshTokenPromise = axiosInstance.post<ReIssue>(END_POINT);
+      const response = await refreshTokenPromise;
+      const { accessToken, expirationTime } = response.data.result;
+
+      if (accessToken && expirationTime) {
+        AuthServices.enrollAuthorizationHeader(accessToken);
+        AuthServices.silentRefresh(expirationTime);
+      }
+    } finally {
+      refreshTokenPromise = undefined;
     }
   },
 
-  enrollAuthorizationHeader: async (accessToken: string) => {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+  enrollAuthorizationHeader: (accessToken: string) => {
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
   },
 
-  deleteAuthorizationHeader: async () => {
-    delete axios.defaults.headers.common['Authorization'];
+  deleteAuthorizationHeader: () => {
+    delete axiosInstance.defaults.headers.common['Authorization'];
   },
 
-  slientRefresh: (JWT_EXPIRRY_TIME: string) => {
-    const refreshTime = Number(JWT_EXPIRRY_TIME) - 60000;
+  silentRefresh: (JWT_EXPIRRY_MINUTE: string) => {
+    const refreshTime = (Number(JWT_EXPIRRY_MINUTE) - 60) * 1000;
     setTimeout(AuthServices.postReIssue, refreshTime);
   },
 
-  getUserInfo: async (endPoint: string) => {},
+  getUserInfo: async (END_POINT: string) => {},
 
   updateUserinfo: () => {},
 };
