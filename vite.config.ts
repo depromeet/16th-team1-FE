7894507f769f1b6a62928b1498b9b121c265f6b2
minh0518 <rubics@naver.com>
2025-03-24 21:16:07 +0900

    chore #119 : vite.config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,46 @@
+/// <reference types="vitest" />
 import path from 'path';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      svgr(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-        manifest: {
-          name: '프로젝트명',
-          short_name: '프로젝트명',
-          description: '프로젝트 설명',
-          theme_color: '#ffffff',
-          lang: 'ko',
-        },
-      }),
-      mkcert(),
-    ],
-    resolve: {
-      alias: [
-        { find: '@', replacement: path.resolve(__dirname, 'src') },
-        { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
-        { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
-        { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
-      ],
-    },
-    server: {
-      port: 3000,
-      host: 'critix.kr',
-    },
-    test: {
-      coverage: {
-        provider: 'v8',
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: '프로젝트명', // 설치 배너에 표시되는 이름
+        short_name: '프로젝트명', // 아이콘 아래에 표시될 이름
+        description: '프로젝트 설명',
+        theme_color: '#ffffff',
+        lang: 'ko',
       },
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setup.ts'],
+    }),
+    mkcert(),
+  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
+      { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
+    ],
+  },
+
+  server: {
+    port: 3000,
+    host: 'critix.kr',
+  },
+  test: {
+    coverage: {
+      provider: 'v8',
     },
-  };
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
 });
