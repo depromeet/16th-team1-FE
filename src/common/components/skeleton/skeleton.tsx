4b21d9381c5f53a01b092098335391bcc02c4688
subsub-e <ab13696802@gmail.com>
2025-03-24 16:59:50 +0900

    refactor #167 : Skeleton컴포넌트 prop에 width, height 추가

--- src/common/components/skeleton/skeleton.tsx
+++ src/common/components/skeleton/skeleton.tsx
@@ -5,9 +5,16 @@ import * as styles from './skeleton.styles';
 //TODO: 디자인시스템 등록 시 반영
 interface SkeletonProps {
   isLoading?: boolean;
+  width?: string;
+  height?: string;
   additionalStyles?: SerializedStyles;
 }
 
-export default function Skeleton({ isLoading = true, additionalStyles }: SkeletonProps) {
-  return <div css={[styles.skeleton(isLoading), additionalStyles]} />;
+export default function Skeleton({
+  isLoading = true,
+  width = '100%',
+  height = '2.2rem',
+  additionalStyles,
+}: SkeletonProps) {
+  return <div css={[styles.skeleton(isLoading, width, height), additionalStyles]} />;
 }
