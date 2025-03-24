c32fec8713ce5d250215b2e400197eb48c026e58
qq8721443 <qq8721443@naver.com>
2025-03-24 22:31:56 +0900

    fix #156 : 포트폴리오 업로드 응답 타입 수정

--- src/features/upload/services/post-portfolio.ts
+++ src/features/upload/services/post-portfolio.ts
@@ -1,9 +1,10 @@
 import { axiosInstance } from '@/common/services/service-config';
+import type { Response } from '@/common/types/response';
 
 import { PortfolioRequest, PortfolioResponse } from '../types/portfolio-types';
 
 export const postPortfolio = async ({ file }: PortfolioRequest) => {
-  const { data } = await axiosInstance.post<PortfolioResponse>(
+  const { data } = await axiosInstance.post<Response<PortfolioResponse>>(
     '/api/v1/files/portfolio',
     {
       file,
