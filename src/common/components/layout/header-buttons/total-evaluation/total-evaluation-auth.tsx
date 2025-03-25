021908463d652d3098f205f99c6dae9cf29702bf
minh0518 <rubics@naver.com>
2025-03-25 16:37:46 +0900

    feat #161 : 프로필 모달 마크업 구현

--- src/common/components/layout/header-buttons/total-evaluation/total-evaluation-auth.tsx
+++ src/common/components/layout/header-buttons/total-evaluation/total-evaluation-auth.tsx
@@ -3,28 +3,36 @@ import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { BaseButton } from '@/common/components/button/base-button';
 
 import AuthProfile from '../auth-profile';
+import SingleProfileModalButton from './single-profile-modal-button';
 
 import * as styles from './total-evaluation-auth.styles';
 
 function TotalEvalutationAuth() {
   return (
-    <div css={styles.container}>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild>
-          <BaseButton>
-            <AuthProfile />
-          </BaseButton>
-        </DropdownMenu.Trigger>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <BaseButton css={styles.positionRelative}>
+          <AuthProfile />
+        </BaseButton>
+      </DropdownMenu.Trigger>
 
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content css={styles.profileContent}>
-            <DropdownMenu.Item>aaaaaa</DropdownMenu.Item>
-            <DropdownMenu.Item>bbbbbb</DropdownMenu.Item>
-            <DropdownMenu.Item>cccccc</DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
-    </div>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content css={styles.profileContent}>
+          <DropdownMenu.Item asChild>
+            <div>
+              <SingleProfileModalButton label="내 계정" />
+              <p css={styles.subRemainFeebackCountPragraph}>이번 달 남은 피드백 2 </p>
+            </div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item asChild>
+            <SingleProfileModalButton label="최근 피드백" />
+          </DropdownMenu.Item>
+          <DropdownMenu.Item asChild>
+            <SingleProfileModalButton label="로그아웃" />
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
 
