3b7a9b4987864a0e61774ddbe1a02f30adf316c5
gabi <70315572+Limgabi@users.noreply.github.com>
2025-03-25 21:58:06 +0900

    style #180 : 사이드바 상태에 따른 푸터 너비 조절

--- src/common/components/layout/footer/footer.tsx
+++ src/common/components/layout/footer/footer.tsx
@@ -1,16 +1,19 @@
+import { useContext } from 'react';
 import { useLocation } from 'react-router';
 
 import Icon from '@/common/components/icon/icon';
 import { PAGE_URL } from '@/common/constants/path';
 import useDeviceType from '@/common/hooks/use-device-type';
+import { SidebarContext } from '@/features/total-evaluation/components/context/sidebar/sidebar-context';
 
 import * as styles from './footer.styles';
 
 export default function Footer() {
   const { pathname } = useLocation();
   const { isMobile } = useDeviceType();
+  const { isSidebarOpen } = useContext(SidebarContext);
 
-  const isShortFooter = pathname.includes(PAGE_URL.TotalEvaluation);
+  const isShortFooter = pathname.includes(PAGE_URL.TotalEvaluation) && isSidebarOpen;
 
   return (
     <div css={styles.footer(isShortFooter)}>
