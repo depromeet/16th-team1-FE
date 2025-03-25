99fad5116594c147c9de1328ec6f8fa31d775748
minh0518 <rubics@naver.com>
2025-03-25 18:50:36 +0900

    refactor #119 : 컴포넌트명 수정(TotalEvaluationAuth -> TotalEvalutationAuthButton)

--- src/common/hooks/use-header-contents.tsx
+++ src/common/hooks/use-header-contents.tsx
@@ -4,7 +4,7 @@ import AuthProfile from '../components/layout/header-buttons/auth-profile';
 import HeaderLogo from '../components/layout/header-buttons/header-logo';
 import LandingAuthButtons from '../components/layout/header-buttons/landing/landing-auth-buttons';
 import LandingScrollSectionButtons from '../components/layout/header-buttons/landing/landing-scroll-section-buttons';
-import TotalEvalutationAuth from '../components/layout/header-buttons/total-evaluation/total-evaluation-auth';
+import TotalEvalutationAuthButton from '../components/layout/header-buttons/total-evaluation/total-evaluation-auth';
 import TotalEvalutationSidebarButtons from '../components/layout/header-buttons/total-evaluation/total-evaluation-sidebar-buttons';
 import TotalEvalutationUserInfo from '../components/layout/header-buttons/total-evaluation/total-evaluation-user-info';
 import { PageLabelKey } from '../constants/path';
@@ -32,7 +32,7 @@ export const useHeaderContents = (pageLabel: PageLabelKey) => {
     TotalEvaluation: {
       left: <TotalEvalutationSidebarButtons />,
       middle: <TotalEvalutationUserInfo />,
-      right: <TotalEvalutationAuth />,
+      right: <TotalEvalutationAuthButton />,
     },
     Login: {
       left: null,
