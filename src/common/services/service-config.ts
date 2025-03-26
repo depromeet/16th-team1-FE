7711fc1647d9e96ccefc56a2b9295af2b1e1faea
minh0518 <rubics@naver.com>
2025-03-26 21:25:45 +0900

    refactor #119 : withCredentials 옵션 위치 수정

--- src/common/services/service-config.ts
+++ src/common/services/service-config.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
-axiosInstance.defaults.withCredentials = true;
 
 axiosInstance.interceptors.response.use(
   (response) => response,
