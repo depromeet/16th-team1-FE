d2f69cbc3e2f4f901b2d447608e1ada19bf22324
minh0518 <rubics@naver.com>
2025-03-26 21:18:27 +0900

    refactor #119 : mkcert 주석 처리

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
+// import mkcert from 'vite-plugin-mkcert';
 import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
@@ -21,7 +21,7 @@ export default defineConfig({
         lang: 'ko',
       },
     }),
-    mkcert(),
+    // mkcert(),
   ],
   resolve: {
     alias: [
@@ -34,7 +34,7 @@ export default defineConfig({
 
   server: {
     port: 3000,
-    host: 'critix.kr',
+    // host: 'critix.kr',
   },
   test: {
     coverage: {
