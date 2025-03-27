a601c7babef1b00e33e9785d7e184ae0d039b74a
subsub-e <ab13696802@gmail.com>
2025-03-27 16:39:53 +0900

    refactor #175 : toastConfig에서 아이콘 직접 넣어주는 방식으로 변경

--- src/common/components/toast/toast-config.tsx
+++ src/common/components/toast/toast-config.tsx
@@ -4,40 +4,43 @@ export type ToastType =
   | 'loginFailure'
   | 'pdfSubmit'
   | 'getFeedbackFailure';
-import { iconTypes } from '@/common/types/icon-types';
+
+import { ReactNode } from 'react';
+
+import Icon from '../icon/icon';
 
 export const toastConfig: Record<
   ToastType,
-  { message: string; duration?: number; icon: iconTypes; iconPosition: 'left' | 'right' }
+  { message: string; duration?: number; icon: ReactNode; iconPosition: 'left' | 'right' }
 > = {
   aiCompleteLarge: {
     message: 'AI 분석이 끝났어요!',
     duration: Infinity,
-    icon: 'arrow-up',
+    icon: <Icon name="arrow-up" />,
     iconPosition: 'right',
   },
   aiCompleteSmall: {
     message: 'AI 분석이 끝났어요!',
     duration: 3000,
-    icon: 'arrow-right-with-tail',
+    icon: <Icon name="arrow-right-with-tail" />,
     iconPosition: 'right',
   },
   loginFailure: {
     message: '로그인에 실패했어요',
     duration: 3000,
-    icon: 'login-fail',
+    icon: <Icon name="login-fail" />,
     iconPosition: 'left',
   },
   pdfSubmit: {
     message: 'PDF 제출이 완료됐어요',
     duration: 3000,
-    icon: 'pdf-submit-success',
+    icon: <Icon name="pdf-submit-success" />,
     iconPosition: 'left',
   },
   getFeedbackFailure: {
     message: '피드백을 불러올 수 없어요',
     duration: Infinity,
-    icon: 'loading',
+    icon: <Icon name="loading" />,
     iconPosition: 'right',
   },
 };
