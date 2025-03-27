a4a92e99e9b3899b0d28362e4304b489c632e2d7
subsub-e <ab13696802@gmail.com>
2025-03-27 16:41:00 +0900

    refactor #175 : 최근 피드백 조회 모달 error UI 추가

--- src/features/profile/components/recent-feedback-modal/recent-feedback-modal.styles.ts
+++ src/features/profile/components/recent-feedback-modal/recent-feedback-modal.styles.ts
@@ -23,7 +23,7 @@ export const closeIconWrapper = css`
   margin: 0.8rem 1rem;
 `;
 
-export const fallbackWrapper = css`
+export const fallbackLoadingWrapper = css`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -32,3 +32,44 @@ export const fallbackWrapper = css`
   gap: 0.8rem;
   padding: 0 1rem;
 `;
+
+export const fallbackErrorWrapper = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  align-self: stretch;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const errorText = withTheme(
+  (theme) => css`
+    ${theme.fonts.SUBTITLE.SUB2_SB};
+    color: ${theme.colors.GRAY[300]};
+    text-align: center;
+  `,
+);
+
+export const buttonWrapper = withTheme(
+  (theme) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.7rem 1.2rem 0.7rem 1.6rem;
+    background: ${theme.colors.GRAY[200]};
+    border-radius: 0.8rem;
+    gap: 0.2rem;
+
+    &:hover {
+      background: ${theme.colors.GRAY[300]};
+    }
+  `,
+);
+
+export const buttonText = withTheme(
+  (theme) => css`
+    ${theme.fonts.SUBTITLE.SUB5_SB}
+    color: ${theme.colors.GRAY[950]};
+  `,
+);
