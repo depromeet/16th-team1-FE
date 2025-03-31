0b198d8482dc30c0237e5552007d16609019af5c
qq8721443 <qq8721443@naver.com>
2025-03-31 23:57:57 +0900

    refactor #195 : 불필요 코드 제거

--- src/common/components/feedback-state-observer/feedback-state-observer.tsx
+++ src/common/components/feedback-state-observer/feedback-state-observer.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 import { getLocalStorage, removeLocalStorage } from '@/common/utils/storage';
