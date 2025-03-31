db0859e8be2f60e2fe1019d0c76d53032c057328
gabi <70315572+Limgabi@users.noreply.github.com>
2025-04-01 08:01:27 +0900

    style #197 : 각 페이지마다 다른 배경색에 맞춰 footer 배경색을 형제 요소에서 추출하여 적용

--- src/common/components/layout/footer/footer.styles.ts
+++ src/common/components/layout/footer/footer.styles.ts
@@ -4,14 +4,16 @@ import { mediaQueries } from '@/assets/styles/device-width';
 import { withTheme } from '@/common/utils/with-theme';
 
 export const footer = withTheme(
-  (theme, isShortFooter: boolean) => css`
+  (theme, isShortFooter: boolean, background: string) => css`
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: ${isShortFooter ? 'calc(100% - 26.4rem)' : '100%'};
-    margin-left: auto;
     padding: 1.2rem 2rem;
     color: ${theme.colors.GRAY[300]} !important;
+    margin-left: auto;
+    margin-top: auto;
+    background-color: ${background};
     ${theme.fonts.SUBTITLE.SUB5_M}
 
     ${mediaQueries.mobile} {
