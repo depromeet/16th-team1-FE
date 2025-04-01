07c5bfbde6adf1641955cc8fa8a9338ee2da76c9
minh0518 <rubics@naver.com>
2025-04-01 23:20:07 +0900

    fix #192 : 로그인 페이지 인증상태 판별 기준 간소화

--- src/features/login/login-page.tsx
+++ src/features/login/login-page.tsx
@@ -17,28 +17,25 @@ function LoginPage() {
   const { executeAuthCycle, createAuthCycle } = useAuthCycle();
 
   const navigate = useNavigate();
-  const { isLogin, userInfo } = useAuthStore();
+  const { isLogin } = useAuthStore();
   const isRollback = useCheckQueryStrings({ rollback: 'true' });
 
   useEffect(() => {
     if (isRollback) return;
-    /** 로그인 페이지 진입 시, 로그인 진행 */
-    if (!isLogin || userInfo === null) {
+
+    if (!isLogin) {
       /**
-       *  스토어에 값이 없을 때:
-       * - 서버 사이드 라우팅일 때, refreshToken은 남아 있기 때문에 재발급 요청 후 /upload로 이동
-       * - 최초 로그인 및 로그아웃 상태일 때, 재발급 요청시 401이 발생하지만 롤백없이 유지(=재로그인 유도)
+       *  스토어에 isLogin이 false:
+       *  - 서버 사이드 라우팅으로 스토어가 비워졌지만, refreshToken은 남아 있을 수 있기 때문에 재발급 요청
+       *  - 만약 여기서 에러 발생한다면 최초 로그인 및 로그아웃 상태(=재로그인 진행)
        */
       const options = createAuthCycle()
-        .withoutRollback() // (로그인 페이지에 있으므로) 롤백 비활성화
         .withMoveOnSuccess() // 성공 시, '/upload' 페이지로 이동
+        .withoutRollback() // 실패 시, (로그인 페이지에 있으므로) 롤백 비활성화로 재로그인 진행
         .build();
 
       executeAuthCycle(options);
-    }
-
-    // 이미 로그인이 돼있을 경우
-    if (isLogin && userInfo !== null) navigate('/upload');
+    } else navigate('/upload');
   }, []);
 
   return (
