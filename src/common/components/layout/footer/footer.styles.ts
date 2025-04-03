f8b6fd0416b1116d7dffc4fdc5f8d4685f9c70b7
Jeongki <qq8721443@naver.com>
2025-04-04 08:50:43 +0900

    [#215] - Layout 스타일 변경 및 페이지 높이 값 제거 (#218)
    * refactor #215 : nth-child 대신 nth-of-type 사용
    
    * refactor #215 : layout 스타일 및 페이지 height 값 제거
    
    * refactor #215 : 정적인 footer height 값 제거
    
    ---------
    
    Co-authored-by: 임가비 <gabi520@naver.com>

--- src/common/components/layout/footer/footer.styles.ts
+++ src/common/components/layout/footer/footer.styles.ts
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 
 import { mediaQueries } from '@/assets/styles/device-width';
-import { FOOTER_HEIGHT } from '@/common/constants/layout';
 import { withTheme } from '@/common/utils/with-theme';
 
 export const footer = withTheme(
@@ -10,7 +9,6 @@ export const footer = withTheme(
     align-items: center;
     justify-content: space-between;
     width: ${isShortFooter ? 'calc(100% - 26.4rem)' : '100%'};
-    height: ${FOOTER_HEIGHT.DEFAULT};
     padding: 1.2rem 2rem;
     color: ${theme.colors.GRAY[300]} !important;
     margin-left: auto;
@@ -19,7 +17,6 @@ export const footer = withTheme(
     ${theme.fonts.SUBTITLE.SUB5_M}
 
     ${mediaQueries.mobile} {
-      height: ${FOOTER_HEIGHT.MOBILE};
       font-size: 1rem;
     }
   `,
