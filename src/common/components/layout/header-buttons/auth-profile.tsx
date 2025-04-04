cc333a3f60d3193259833b3e30bde6caaaef3224
Cho Minho <78631876+minh0518@users.noreply.github.com>
2025-04-04 22:32:15 +0900

    [#229] - 더미 프로필 추가, 사이드바 텍스트 초과 처리  (#230)
    * feat #229 : 구글 이미지 fallback UI 추가
    
    * feat #229 : 사이드바 컨텐츠가 너비를 초과할 시 말줄임표(ellipsis) 처리

--- src/common/components/layout/header-buttons/auth-profile.tsx
+++ src/common/components/layout/header-buttons/auth-profile.tsx
@@ -5,11 +5,19 @@ import * as styles from './auth-profile.styles';
 
 function AuthProfile() {
   const { isLogin, userInfo } = useAuthStore();
+
   const { isMobile } = useDeviceType();
   if (!isLogin || userInfo === null) return null;
+
   return (
     <div css={styles.container(isMobile)}>
-      <img src={userInfo.profileImageUrl} alt="구글 프로필" css={styles.img} />
+      {userInfo.profileImageUrl ? (
+        <img src={userInfo.profileImageUrl} alt="구글 프로필" css={styles.img} />
+      ) : (
+        <div css={styles.dummyProFile}>
+          <p css={styles.dummyProfileName}>{userInfo.email[0]}</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/features/total-evaluation/components/feedback-sidebar/feedback-sidebar.styles.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const overFlowTextWrapper = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
