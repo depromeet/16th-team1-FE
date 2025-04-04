a896d0c59699bbac68eb259d196fc544ed1222b5
minh0518 <rubics@naver.com>
2025-04-04 16:23:41 +0900

    feat #225 : SignIn 버튼 제거

--- src/common/components/layout/header-buttons/landing/landing-auth-buttons.tsx
+++ src/common/components/layout/header-buttons/landing/landing-auth-buttons.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router';
 import { css } from '@emotion/react';
 
 import { Button } from '@/common/components/button/Button';
-import useDeviceType from '@/common/hooks/use-device-type';
 import { useAuthStore } from '@/store/user-auth';
 
 import AuthProfileModalDropdown from '../auth-profile-modal-dropdown';
@@ -12,41 +11,25 @@ import * as styles from './landing-auth-buttons.styles';
 
 function LandingAuthButtons() {
   const navigate = useNavigate();
-  const { isMobile } = useDeviceType();
+
   const { isLogin } = useAuthStore();
   return (
     <div css={styles.container}>
       {isLogin ? (
         <AuthProfileModalDropdown />
       ) : (
-        <>
-          {!isMobile && (
-            <Button
-              size="small"
-              variant="text"
-              usage="text"
-              onClick={() => navigate('/login')}
-              css={css`
-                width: 8.1rem;
-                height: 3.4rem;
-              `}
-            >
-              Login
-            </Button>
-          )}
-
-          <Button
-            size="small"
-            variant="primary"
-            usage="text"
-            css={css`
-              width: 8.1rem;
-              height: 3.4rem;
-            `}
-          >
-            Sign up
-          </Button>
-        </>
+        <Button
+          size="small"
+          variant="primary"
+          usage="text"
+          onClick={() => navigate('/login')}
+          css={css`
+            width: 8.1rem;
+            height: 3.4rem;
+          `}
+        >
+          Login
+        </Button>
       )}
     </div>
   );
