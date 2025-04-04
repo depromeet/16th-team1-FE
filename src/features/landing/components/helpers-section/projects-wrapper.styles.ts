fa10203d6c0f01cf8e4ca26408e663902d426790
Jeongki <qq8721443@naver.com>
2025-04-04 21:37:49 +0900

    [#228] - 랜딩 페이지 레이아웃 시프트 수정(gsap 설정 수정)
    [#219] - 랜딩 페이지 레이아웃 시프트 수정(gsap 설정 수정)

--- src/features/landing/components/helpers-section/projects-wrapper.styles.ts
+++ src/features/landing/components/helpers-section/projects-wrapper.styles.ts
@@ -1,22 +1,14 @@
 import { css } from '@emotion/react';
 
-import { mediaQueries } from '@/assets/styles/device-width';
-
 export const projectContainer = css`
   display: flex;
   overflow: hidden;
-  width: 100vw;
+  width: 100%;
   max-width: 103rem;
-
-  ${mediaQueries.tablet} {
-    margin: 0 2.4rem;
-  }
-
-  ${mediaQueries.mobile} {
-    margin: 0 2rem;
-  }
 `;
 
 export const projectScrollContainer = css`
-  width: 100%;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
 `;
