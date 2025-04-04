d44fbdb7c40508e2d5fc3551676500fd7157defb
minh0518 <rubics@naver.com>
2025-04-04 16:05:10 +0900

    feat #225 : 헤더 로고 버튼 시 랜딩페이지 이동 로직 추가

--- src/common/components/layout/header-buttons/total-evaluation/total-evaluation-sidebar-buttons.tsx
+++ src/common/components/layout/header-buttons/total-evaluation/total-evaluation-sidebar-buttons.tsx
@@ -1,6 +1,7 @@
-import Icon from '@/common/components/icon/icon';
 import { useSidebarContext } from '@/features/total-evaluation/components/context/sidebar/use-sidebar-context';
 import { SidebarOpenButton } from '@/features/total-evaluation/components/custom-buttons/sidebar-open-button';
+
+import HeaderLogo from '../header-logo';
 
 import * as styles from './total-evaluation-sidebar-buttons.styles';
 
@@ -9,7 +10,7 @@ function TotalEvalutationSidebarButtons() {
   return (
     <div css={styles.container}>
       <SidebarOpenButton onClick={() => setIsSidebarOpen((prev) => !prev)} />
-      <Icon name="logo-full-header-desktop" />
+      <HeaderLogo />
     </div>
   );
 }
