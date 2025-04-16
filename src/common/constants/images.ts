1d5d2638c9198622168e084df54981a21bd7b4d0
subsub-e <ab13696802@gmail.com>
2025-04-16 21:19:33 +0900

    fix #281 :  이미지 파일 경로 import하는 방식으로 수정 및 파싱하는 스크립트 코드 수정

--- src/common/constants/images.ts
+++ src/common/constants/images.ts
@@ -1,25 +1,49 @@
+import detailevaluationchart from '@/assets/images/detail-evaluation-chart.png';
+import gradesmall from '@/assets/images/grade-small.png';
+import grade from '@/assets/images/grade.png';
+import grammar from '@/assets/images/grammar.png';
+import improvement01 from '@/assets/images/improvement-01.png';
+import improvement02 from '@/assets/images/improvement-02.png';
+import improvement03 from '@/assets/images/improvement-03.png';
+import improvement04 from '@/assets/images/improvement-04.png';
+import improvement05 from '@/assets/images/improvement-05.png';
+import loadingprojectevaluation from '@/assets/images/loading-project-evaluation.png';
+import loadingtotalevaluationsmall from '@/assets/images/loading-total-evaluation-small.png';
+import loadingtotalevaluation from '@/assets/images/loading-total-evaluation.png';
+import logic from '@/assets/images/logic.png';
+import project01 from '@/assets/images/project-01.png';
+import project02 from '@/assets/images/project-02.png';
+import project03 from '@/assets/images/project-03.png';
+import project04 from '@/assets/images/project-04.png';
+import readability from '@/assets/images/readability.png';
+import redundancy from '@/assets/images/redundancy.png';
+import stepcard01 from '@/assets/images/step-card-01.png';
+import stepcard02 from '@/assets/images/step-card-02.png';
+import totalevaluation from '@/assets/images/total-evaluation.png';
+import translation from '@/assets/images/translation.png';
+
 export const IMAGES: Record<string, string> = {
-  'detail-evaluation-chart': '../../../src/assets/images/detail-evaluation-chart.png',
-  'grade-small': '../../../src/assets/images/grade-small.png',
-  grade: '../../../src/assets/images/grade.png',
-  grammar: '../../../src/assets/images/grammar.png',
-  'improvement-01': '../../../src/assets/images/improvement-01.png',
-  'improvement-02': '../../../src/assets/images/improvement-02.png',
-  'improvement-03': '../../../src/assets/images/improvement-03.png',
-  'improvement-04': '../../../src/assets/images/improvement-04.png',
-  'improvement-05': '../../../src/assets/images/improvement-05.png',
-  'loading-project-evaluation': '../../../src/assets/images/loading-project-evaluation.png',
-  'loading-total-evaluation-small': '../../../src/assets/images/loading-total-evaluation-small.png',
-  'loading-total-evaluation': '../../../src/assets/images/loading-total-evaluation.png',
-  logic: '../../../src/assets/images/logic.png',
-  'project-01': '../../../src/assets/images/project-01.png',
-  'project-02': '../../../src/assets/images/project-02.png',
-  'project-03': '../../../src/assets/images/project-03.png',
-  'project-04': '../../../src/assets/images/project-04.png',
-  readability: '../../../src/assets/images/readability.png',
-  redundancy: '../../../src/assets/images/redundancy.png',
-  'step-card-01': '../../../src/assets/images/step-card-01.png',
-  'step-card-02': '../../../src/assets/images/step-card-02.png',
-  'total-evaluation': '../../../src/assets/images/total-evaluation.png',
-  translation: '../../../src/assets/images/translation.png',
+  'detail-evaluation-chart': detailevaluationchart,
+  'grade-small': gradesmall,
+  grade: grade,
+  grammar: grammar,
+  'improvement-01': improvement01,
+  'improvement-02': improvement02,
+  'improvement-03': improvement03,
+  'improvement-04': improvement04,
+  'improvement-05': improvement05,
+  'loading-project-evaluation': loadingprojectevaluation,
+  'loading-total-evaluation-small': loadingtotalevaluationsmall,
+  'loading-total-evaluation': loadingtotalevaluation,
+  logic: logic,
+  'project-01': project01,
+  'project-02': project02,
+  'project-03': project03,
+  'project-04': project04,
+  readability: readability,
+  redundancy: redundancy,
+  'step-card-01': stepcard01,
+  'step-card-02': stepcard02,
+  'total-evaluation': totalevaluation,
+  translation: translation,
 };
