ebd361e6ec29ea897d4a570a3040eb39e44f4ac2
subsub-e <ab13696802@gmail.com>
2025-04-18 18:27:00 +0900

    fix #283 : 디자이너 포트폴리오 판단 로직 추가

--- src/features/upload/components/portfolio-upload/portfolio-upload.tsx
+++ src/features/upload/components/portfolio-upload/portfolio-upload.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { AxiosError } from 'axios';
+
 import { getLocalStorage, setLocalStorage } from '@/common/utils/storage';
 import { useGetStartFeedback } from '@/features/feedback/services/use-get-start-feedback';
 import { useFeedbackStore } from '@/store/feedback';
@@ -27,54 +29,54 @@ export default function PortfolioUpload() {
       progress={progress}
       remainCount={remainingCount?.result.remainCount}
       onSubmit={async ({ file }) => {
-        try {
-          const formData = new FormData();
-          formData.append('file', file);
+        const formData = new FormData();
+        formData.append('file', file);
 
-          changeState('IDLE');
+        changeState('IDLE');
 
-          await postPortfolio(
-            {
-              file,
-              onUploadProgress: ({ loaded, total }) => {
-                if (!total) return;
-                const percentage = Math.round((loaded / total) * 100); // 업로드 진행률
-                setProgress(percentage);
-              },
+        await postPortfolio(
+          {
+            file,
+            onUploadProgress: ({ loaded, total }) => {
+              if (!total) return;
+              const percentage = Math.round((loaded / total) * 100); // 업로드 진행률
+              setProgress(percentage);
             },
-            {
-              onSuccess: async (data) => {
-                const { id } = data.result;
+          },
+          {
+            onSuccess: async (data) => {
+              const { id } = data.result;
 
-                const { feedbackId } = await startFeedback(id);
+              const { feedbackId } = await startFeedback(id);
 
-                changeState('PENDING', feedbackId);
+              changeState('PENDING', feedbackId);
 
-                const email = userInfo?.email || '';
-                const prevFeedbacks = JSON.parse(getLocalStorage('feedbacks') || '{}');
-                setLocalStorage(
-                  'feedbacks',
-                  JSON.stringify({
-                    ...prevFeedbacks,
-                    [email]: feedbackId,
-                  }),
-                );
-              },
-              onError: () => {
+              const email = userInfo?.email || '';
+              const prevFeedbacks = JSON.parse(getLocalStorage('feedbacks') || '{}');
+              setLocalStorage(
+                'feedbacks',
+                JSON.stringify({
+                  ...prevFeedbacks,
+                  [email]: feedbackId,
+                }),
+              );
+            },
+            onError: (error) => {
+              if (error instanceof AxiosError && error.response) {
+                const statusCode = error.response.data.code;
+                if (statusCode === 'FILE_400') {
+                  changeState('ONLY_DESIGNER_PORTFOLIO');
+                } else {
+                  changeState('ERROR');
+                }
+              } else {
                 changeState('ERROR');
+              }
 
-                setProgress(undefined);
-              },
+              setProgress(undefined);
             },
-          );
-        } catch (error) {
-          if (error instanceof Error) {
-            // TODO: 업로드 실패 시 액션
-            changeState('ERROR');
-
-            setProgress(undefined);
-          }
-        }
+          },
+        );
       }}
     />
   );
